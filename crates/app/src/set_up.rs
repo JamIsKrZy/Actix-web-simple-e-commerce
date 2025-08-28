85af9d5253646517c0cf7417a95d6c181f43cb7f
JusMe <jamessamson322@gmail.com>
2025-08-28 16:11:34 +0800

    added cors

--- crates/app/src/set_up.rs
+++ crates/app/src/set_up.rs
@@ -44,13 +44,8 @@ pub mod shuttle {
             let allowed_origins = allowed_origins.leak();
 
             for o in allowed_origins.split(",") {
-                cors = cors.allowed_origin(o);
-                log::info!("Allowed Origin: {}", o);
-                println!("Allowed Origin: {}", o);
+                cors = cors.allowed_origin(o)
             }
-        } else {
-            log::info!("ALLOWED_ORIGIN not found!");
-            println!("ALLOWED_ORIGIN not found!");
         }
 
         cors.allow_any_method().allowed_headers(&[
