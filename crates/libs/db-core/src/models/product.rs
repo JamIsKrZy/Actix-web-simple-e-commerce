440dc06a41cefcc39b6a6a24609e526a23bf28fc
JusMe <jamessamson322@gmail.com>
2025-08-28 16:11:34 +0800

    added cors

--- crates/libs/db-core/src/models/product.rs
+++ crates/libs/db-core/src/models/product.rs
@@ -7,8 +7,6 @@ use sqlx::{
 };
 use uuid::Uuid;
 
-use utoipa::ToSchema;
-
 use crate::{
     models::{Pagination, ProductStatus, QueryFilterBuilder, QueryResult},
     utils::DbPoolExtract,
@@ -20,14 +18,14 @@ use crate::{
 
 // region:    --- Schemas
 
-#[derive(Debug, FromRow, Serialize, Deserialize, ToSchema)]
-pub struct ProductID {
+#[derive(Debug, FromRow, Serialize, Deserialize)]
+pub struct ProductEssential {
     pub name: String,
     pub id: i32,
 }
 
-#[derive(Debug, Deserialize, ToSchema)]
-pub struct AddProduct {
+#[derive(Debug, Deserialize)]
+pub struct NewProduct {
     pub name: String,
     pub description: Option<String>,
     pub price: Decimal,
@@ -35,17 +33,7 @@ pub struct AddProduct {
     pub status: ProductStatus,
 }
 
-#[derive(Debug, FromRow, Deserialize, Serialize, ToSchema)]
-pub struct ProductDescription {
-    pub id: i32,
-    pub name: String,
-    pub status: ProductStatus,
-    pub description: Option<String>,
-    pub price: Decimal,
-    pub stocks: i32,
-}
-
-#[derive(Debug, Deserialize, ToSchema)]
+#[derive(Debug, Deserialize)]
 pub struct UpdateProduct {
     pub id: i32,
     pub name: Option<String>,
@@ -54,14 +42,7 @@ pub struct UpdateProduct {
     pub stock: Option<i32>,
 }
 
-#[derive(Debug, FromRow, Deserialize, Serialize, ToSchema)]
-pub struct ForPublicProductList {
-    pub id: i32,
-    pub name: String,
-    pub price: Decimal,
-}
-
-#[derive(Debug, FromRow, Serialize, Deserialize, ToSchema)]
+#[derive(Debug, FromRow, Serialize, Deserialize)]
 pub struct ForAdminProductList {
     pub id: i32,
     pub name: String,
@@ -76,7 +57,7 @@ pub struct ForAdminProductList {
     pub edited_at: Option<NaiveDateTime>,
 }
 
-#[derive(Debug, Deserialize, ToSchema)]
+#[derive(Debug, Deserialize)]
 pub struct PageFilter {
     min_price: Option<i32>,
     max_price: Option<i32>,
@@ -101,49 +82,10 @@ impl QueryFilterBuilder for PageFilter {
 pub struct Bmc;
 
 impl Bmc {
-    pub async fn get_product_by_id(
-        id: i32,
-        db: &impl DbPoolExtract<Postgres>,
-    ) -> QueryResult<Option<ProductDescription>> {
-        let product = sqlx::query_as!(
-            ProductDescription,
-            "SELECT
-                id, name, status as \"status:ProductStatus\",
-                price, description, stocks
-            FROM products
-            WHERE id = $1",
-            id
-        )
-        .fetch_optional(db.pool())
-        .await
-        .map_err(|e| crate::DbError::FailedSelect { log: e.to_string() })?;
-
-        Ok(product)
-    }
-
-    pub async fn set_product_status(
-        id: i32,
-        status: ProductStatus,
-        db: &impl DbPoolExtract<Postgres>,
-    ) -> QueryResult<()> {
-        let _ = sqlx::query!(
-            "UPDATE products
-            SET status = $1
-            WHERE id = $2",
-            status as ProductStatus,
-            id
-        )
-        .execute(db.pool())
-        .await
-        .map_err(|e| crate::DbError::FailedPatch { log: e.to_string() })?;
-
-        Ok(())
-    }
-
     pub async fn essential_list(
         page: Pagination<PageFilter>,
         db: &impl DbPoolExtract<Postgres>,
-    ) -> QueryResult<Vec<ProductID>> {
+    ) -> QueryResult<Vec<ProductEssential>> {
         let mut query = sqlx::QueryBuilder::new(
             "SELECT
                 name, id
@@ -166,7 +108,7 @@ impl Bmc {
         who: impl AsRef<Uuid>,
         db: &impl DbPoolExtract<Postgres>,
     ) -> QueryResult<()> {
-        let AddProduct {
+        let NewProduct {
             name,
             description,
             price,
@@ -209,29 +151,6 @@ impl Bmc {
         .map_err(|e| crate::DbError::FailedDelete { log: e.to_string() })?;
 
         Ok(())
-    }
-
-    pub async fn public_list<T: QueryFilterBuilder>(
-        page: Pagination<T>,
-        db: &impl DbPoolExtract<Postgres>,
-    ) -> QueryResult<Vec<ForPublicProductList>> {
-        let mut query = QueryBuilder::new(
-            "Select 
-                p.id,
-                p.name,
-                p.price
-            FROM products",
-        );
-
-        page.append_query(&mut query);
-
-        let list = query
-            .build_query_as::<ForPublicProductList>()
-            .fetch_all(db.pool())
-            .await
-            .map_err(|e| crate::DbError::FailedSelect { log: e.to_string() })?;
-
-        Ok(list)
     }
 
     pub async fn get_full_list<T: QueryFilterBuilder>(
